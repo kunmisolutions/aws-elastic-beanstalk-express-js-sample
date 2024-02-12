81ec2266ee2c62874165cee72dcbb34468175988
Johanna <kunmisolutions@gmail.com>
2024-02-12 11:41:07 -0500

    My_First_CICD!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My First CICD Pipeline!'));
+app.get('/', (req, res) => res.send('CICD Pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
